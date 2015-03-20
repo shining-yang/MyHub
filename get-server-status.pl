51ce15e044688000e7c5488894e6974edeab3da3
saturn@mint <y.s.n@live.com>
2015-03-20 17:26:27 +0800

    Add script to get-server-status

---
+++ get-server-status.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl -w
+use strict;
+
+my $cmd = 'curl -s -S -i http://172.18.190.38:9502/api/v1/server/status';
+my ($count) = @ARGV;
+if (!defined($count)) {
+	$count = 10;
+}
+
+sub queryServerStatusSequentially {
+	for (my $i = 0; $i < $count; $i++) {
+		print "$cmd\n";
+		print `$cmd`, "\n";
+	}
+}
+
+sub queryServerStatusConcurrently {
+	my $cmdAll = '';
+	for (my $i = 0; $i < $count; $i++) {
+		$cmdAll .= $cmd;
+		$cmdAll .= ';';
+	}
+
+	print $cmdAll, "\n";
+	print `$cmdAll`, "\n";
+}
+
+if ((@ARGV == 2) && ($ARGV[0] =~ /^\d+$/) &&
+   	(($ARGV[1] =~ /^s$/i) || ($ARGV[1] =~ /^c$/i) || ($ARGV[1] =~ /^sequentially$/) || ($ARGV[1] =~ /^concurrently$/))) { 
+	if (($ARGV[1] =~ /^s$/i) || ($ARGV[1] =~ /^sequentially$/)) {
+		queryServerStatusSequentially();
+   	} else {
+ 		queryServerStatusConcurrently();
+	}
+} else {
+	print("Description:\n");
+	print("  Query License Server's status for a number of times.\n");
+	print("Arguments:\n");
+	print("  [number-of-queries]  [Sequentially | Concurrently]\n");
+	print("Example:\n");
+	print("  ./thisPerlScript  100  c\n");
+}

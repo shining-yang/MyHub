336aad9beb98a2aa7a2ea7e7688de6113587b290
saturn@mint <y.s.n@live.com>
2015-05-29 17:17:37 +0800

    Modify parameters for test

--- Snmp-Simulator/a.snmpsimd.sh
+++ Snmp-Simulator/a.snmpsimd.sh
@@ -2,9 +2,9 @@
 
 # Generate simulator argument files
 echo "### Building simulator arguments ..."
-perl build_sim_args.pl 50000 50999
+perl build_sim_args.pl 50000 50009
 
 # Start snmp simulators
 echo "### Starting SNMP Simulators ..."
-snmpsimd  --data-dir=./snmprec/DES-3028  --args-from-file=sim_port_50000_50999.txt 
+snmpsimd  --v2c-arch  --data-dir=./snmprec/DES-3028/public  --args-from-file=sim_port_50000_50009.txt 
 

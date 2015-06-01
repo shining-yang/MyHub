589c76e7f075c9f942d5a46f86e1be6a272d4ed1
saturn@mint <y.s.n@live.com>
2015-06-01 12:00:42 +0800

    Simplify for test

--- Snmp-Simulator/a.snmpsimd.sh
+++ Snmp-Simulator/a.snmpsimd.sh
@@ -2,9 +2,9 @@
 
 # Generate simulator argument files
 echo "### Building simulator arguments ..."
-perl build_sim_args.pl 50000 50009
+perl build_sim_args.pl 50000 50001
 
 # Start snmp simulators
 echo "### Starting SNMP Simulators ..."
-snmpsimd  --v2c-arch  --data-dir=./snmprec/DES-3028/public  --args-from-file=sim_port_50000_50009.txt 
+snmpsimd  --v2c-arch  --data-dir=./snmprec/DES-3028/public  --args-from-file=sim_port_50000_50001.txt 
 

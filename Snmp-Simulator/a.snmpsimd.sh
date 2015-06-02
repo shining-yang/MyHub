689a1622db60296953cf816f1bdc8f599e6e0341
Shining Yang <y.s.n@live.com>
2015-06-02 22:33:25 +0800

    update test case

--- Snmp-Simulator/a.snmpsimd.sh
+++ Snmp-Simulator/a.snmpsimd.sh
@@ -5,13 +5,13 @@ echo "### Removing tmp directory to start over ...";
 rm -rf tmp/
 
 # Generate simulator argument files
-echo "\n### Building simulator arguments ..."
+echo "### Building simulator arguments ..."
 perl build-simulator-args.pl 50000 50002
 
 # Start snmp simulators
-echo "\n### Building SNMP transport id ..."
+echo "### Building SNMP transport id ..."
 perl build-transport-id.pl ./static/DES-3028 1000 4
 
 # READY to invoke
-echo "\n### Run SNMP simulators now ...\n";
+echo "### Run SNMP simulators now ...";
 snmpsimd  --v2c-arch  --data-dir=./tmp  --transport-id-offset=1000  --args-from-file=./tmp/sim_port_50000_50002.txt 

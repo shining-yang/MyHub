eb8d0addff51f100085c99ce12e5e40a07279219
saturn@mint <y.s.n@live.com>
2015-06-02 18:15:17 +0800

    Remove some code to make it simple & plain

--- Snmp-Simulator/mac.pl
+++ Snmp-Simulator/mac.pl
@@ -3,10 +3,10 @@
 #use warnings;
 use strict;
 
-my $mac_base = '001a1a1a0000';
 my $mac_prefix = '001a1a1a'; # support 65535 MACs at most
 
 # Better efficiency without bounds checking. Assume that ARGV is available
+
 # my ($data_file) = @ARGV;
 # $num = 0 unless $num; 
 # print "$num is: ", $num, "\n";
@@ -16,9 +16,6 @@ my $mac_prefix = '001a1a1a'; # support 65535 MACs at most
 #
 sub generate_mac_str {
 	my ($id) = @_;
-	# $id += 0;
-	# $id = 0 if $id < 0;
-	# $id = 65535 if $id > 65535;
 	return sprintf("%s%04x", $mac_prefix, $id);
 }
 

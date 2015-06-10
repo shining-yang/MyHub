2871c95dc37ab63bfcaf6af122dbddb26908ed49
Shining <y.s.n@live.com>
2015-06-10 15:13:29 +0800

    generate snmprec with mac specified directly

--- Snmp-Simulator/build-transport-id.pl
+++ Snmp-Simulator/build-transport-id.pl
@@ -7,15 +7,42 @@ use strict;
 #
 ####################################################################
 
+my $mac_prefix = '001a1a1a'; # mac prefix for simulated snmp devices
+my $mac_placeholder = '001c1c1c1c1c';
 my $pwd = `pwd`;
 chomp $pwd;
+
+
+sub generate_mac_by_id {
+	my ($id) = @_;
+	$id = 0 unless defined $id;
+	$id &= 0xFFFF;
+	return sprintf("%s%04x", $mac_prefix, $id);
+}
+
+sub replace_file_content_string {
+	my ($file, $old_str, $new_str) = @_;
+	open(F, "<", $file) or die("Fail to open file: $!");
+	my @file_contents = <F>;
+	close F;
+	
+	# print("Parameters: old-str: ", $old_str, ", new_str: ", $new_str, "\n");
+	
+	foreach (@file_contents) {
+		s/$old_str/$new_str/g;
+	}
+	
+	open(F, ">", $file) or die("Fail to open file for write: $!");
+	print F @file_contents;
+	close F;
+}
 
 #
 # NOTE: 
 # Without any validations fro simplicity and efficiency.
 # You MUST provide the required arguments correctly.
 #
-sub create_symbolic_links {
+sub create_snmp_records {
 	my ($path, $start_id, $total_count) = @_;
 	if (substr($path, -1) ne '/') {
 		$path .= '/';
@@ -34,16 +61,28 @@ sub create_symbolic_links {
 		`mkdir tmp`;
 	}
 
+	##########################################################################
+	# CURRENTLY supported device types, THESE words must be directory names
+	##########################################################################
+	my @support_types = qw(DAP-3690 DES-3028);
+
 	my $base_file = 'base.snmprec.bak';
 	my $file_name_prefix = '1.3.6.1.6.1.1.';
 	my $file_name_extension = '.snmprec';
 
 	for (my $id = $start_id; $id < $start_id + $total_count; $id++) {
+		my $support_index = $id % (@support_types); # modular
+
 		# my $cmd = "ln -s ";
 		my $cmd = "cp ";
 		$cmd .= $path;
+		$cmd .= '/';
+		$cmd .= $support_types[$support_index];
+		$cmd .= '/';
 		$cmd .= $base_file;
+		
 		$cmd .= ' ';
+		
 		$cmd .= $path_output;
 		$cmd .= $file_name_prefix;
 		$cmd .= $id;
@@ -51,6 +90,11 @@ sub create_symbolic_links {
 
 		# print $cmd, "\n";
 		`$cmd`;
+		
+		### Replace file content with specific string, lazy method.
+		my $new_mac = &generate_mac_by_id($id);
+		&replace_file_content_string($path_output.$file_name_prefix.$id.$file_name_extension,
+			$mac_placeholder, $new_mac);
 	}
 }
 
@@ -61,14 +105,13 @@ sub print_usage {
 	print("\tTHIS-SCRIPT  ./static/DES-3028  10  2\n");
 }
 
-
 if (@ARGV < 3) {
 	print_usage();
 	exit(0);
 }
 
-print("Creating symbolic links ...\n");
-my ($path, $i, $n) = @ARGV;
-&create_symbolic_links($path, $i, $n);
+print("Creating transport-based snmp records ...\n");
+my ($path, $start_id, $count) = @ARGV;
+&create_snmp_records($path, $start_id, $count);
 print("Operation commpleted.\n");
 

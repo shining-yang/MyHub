be79c53cf9d53ce8e6528d473c3964304b479737
Shining <y.s.n@live.com>
2015-06-10 15:14:07 +0800

    generate script based on directories

--- Snmp-Simulator/build-multiple-instance-scripts.pl
+++ Snmp-Simulator/build-multiple-instance-scripts.pl
@@ -1,16 +1,12 @@
 #!/usr/bin/perl -w
 use strict;
 
-my $script1 = 'perl build-simulator-args.pl ';
-my $script2 = 'perl build-transport-id.pl ./static/DES-3028 ';
-my $script3 = 'snmpsimd --v2c-arch --data-dir=./tmp --transport-id-offset=';
-
-
 my ($hostip) = @ARGV;
-my $startport = 50000;
-my $count = 100;
-my $idoffset = 1000;
-my $file = 'sim';
+my $start_port = 50000;		# Start port NO.
+my $port_count = 100;		# Number of ports supported within each script file
+my $id_offset = 1000;		# Transport id offset for SNMPSIMD
+my $script_file = 'sim';	# prefix of generated script file name
+my $script_file_count = 10;	# number of script to generate
 
 if (@ARGV < 1) {
 	print "Usage:\n";
@@ -18,18 +14,23 @@ if (@ARGV < 1) {
 	exit(0);
 }
 
-for (my $n = 0; $n < 10; $n++) {
-	open(F, ">", $file.$n) or die "Fail to open file: $!";
-	print F $script1.$hostip." ".$startport." ".($startport + $count - 1)."\n\n";
 
-	print F $script2.$idoffset." ".$count."\n\n";
+my $script1 = 'perl build-simulator-args.pl ';
+my $script2 = 'perl build-transport-id.pl ./static ';
+my $script3 = 'snmpsimd --v2c-arch --data-dir=./tmp --transport-id-offset=';
 
-	print F $script3.$idoffset." --args-from-file=./tmp/sim_port_".$startport."_".($startport + $count - 1).".txt\n\n";
+for (my $n = 0; $n < $script_file_count; $n++) {
+	open(F, ">", $script_file.$n) or die "Fail to open file: $!";
+	print F $script1.$hostip." ".$start_port." ".($start_port + $port_count - 1)."\n\n";
+	print F $script2.$id_offset." ".$port_count."\n\n";
+	print F $script3.$id_offset." --args-from-file=./tmp/sim_port_".$start_port."_".($start_port + $port_count - 1).".txt\n\n";
 	close F;
 
-	$startport += $count;
-	$idoffset += $count;
+	$start_port += $port_count;
+	$id_offset += $port_count;
 	
-	my $cmd = "chmod +x ".$file.$n;
+	my $cmd = "chmod +x ".$script_file.$n;
 	`$cmd`;
 }
+
+print "Operation completes.\n";

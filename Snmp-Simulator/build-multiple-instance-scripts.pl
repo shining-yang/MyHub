81abf9f57ceb3fb82bbb56d6a2dd2670ec1d6605
Shining <y.s.n@live.com>
2015-06-12 15:32:12 +0800

    Changed the generated file names

--- Snmp-Simulator/build-multiple-instance-scripts.pl
+++ Snmp-Simulator/build-multiple-instance-scripts.pl
@@ -7,12 +7,14 @@ use strict;
 # as they stand for HOW-MANY-SNMP-DEVICES each snmpsimd instance will simulate and
 # HOW-MANY-INSTANCE-SCRIPT will be generated, respectively.
 #
-my ($hostip)	= @ARGV;
+my ($host_ip)	= @ARGV;		# IPv4 of local host that will be listened 
+
 my $start_port	= 50000;		# Start port NO.
-my $port_count	= 500;			# Number of ports supported within each script file
+my $port_count	= 200;			# Number of ports supported within each script file
+								# ALSO, it's the number of the simulated devices within an instance
 my $id_offset	= 1000;			# Transport id offset for SNMPSIMD
-my $script_file	= 'sim';		# prefix of generated script file name
-my $script_file_count = 10;		# number of script to generate
+my $output_file_prefix = 'sim-';	# prefix of generated script file name
+my $output_file_count = 10;		# number of script to generate
 
 if (@ARGV < 1) {
 	print "Usage:\n";
@@ -27,18 +29,25 @@ my $script1 = 'perl build-simulator-args.pl ';
 my $script2 = 'perl build-transport-id.pl ./static ';
 my $script3 = 'snmpsimd --v2c-arch --data-dir=./tmp --transport-id-offset=';
 
-for (my $n = 0; $n < $script_file_count; $n++) {
-	open(F, ">", $script_file.$n) or die "Fail to open file: $!";
-	print F $script1.$hostip." ".$start_port." ".($start_port + $port_count - 1)."\n\n";
-	print F $script2.$id_offset." ".$port_count."\n\n";
-	print F $script3.$id_offset." --args-from-file=./tmp/sim_port_".$start_port."_".($start_port + $port_count - 1).".txt\n\n";
+for (my $n = 0; $n < $output_file_count; $n++) {
+	my $output_file_name = $output_file_prefix.$start_port."-".($start_port + $port_count - 1); # the output file name
+
+	open(F, ">", $output_file_name) or die "Fail to open file: $!";
+	print F "#!/bin/sh\n";
+	print F "# This is an auto-generated file which will be used to launch as an SNMP simulator.\n";
+	print F "\n# 1. Generate SNMP simulator argument files\n";
+	print F $script1.$host_ip." ".$start_port." ".($start_port + $port_count - 1)."\n";
+	print F "\n# 2. Build SNMP simulator transport id\n";
+	print F $script2.$id_offset." ".$port_count."\n";
+	print F "\n# 3. Run simulator\n";
+	print F $script3.$id_offset." --args-from-file=./tmp/sim_port_".$start_port."_".($start_port + $port_count - 1).".txt\n";
 	close F;
 
-	my $cmd = "chmod +x ".$script_file.$n;
+	my $cmd = "chmod +x ".$output_file_name;
 	`$cmd`;
 
 	$start_port += $port_count;
 	$id_offset += $port_count;
 }
 
-print "Operation completes.\n";
+print "SNMP simulator startup scripts generated. Please check them out.\n";

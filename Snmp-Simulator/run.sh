28de6941e5c087f143879f09af16abb631f74055
Shining <y.s.n@live.com>
2015-06-12 15:33:06 +0800

    Rename RUN.SH to run.sh

---
+++ Snmp-Simulator/run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+###########################################################################
+#
+# Launch snmpsimd to simulate 200 SNMP devices which use port 50000~50199
+# y.s.n@live.com, 2015-06-02, SNMP-Simulation
+#
+##########################################################################
+
+
+# REMOVE the tmp directory so that we can start over again
+echo "### Removing tmp directory to start over ...";
+rm  -rf  tmp/
+
+# Generate simulator argument files
+echo "### Building simulator arguments ..."
+perl  build-simulator-args.pl  172.18.190.46  50000  50199
+
+# Start snmp simulators
+echo "### Building SNMP transport id ..."
+perl  build-transport-id.pl  ./static  1000  200
+
+# READY to invoke
+echo "### Run SNMP simulators now ...";
+snmpsimd  --v2c-arch  --data-dir=./tmp  --transport-id-offset=1000  --args-from-file=./tmp/sim_port_50000_50199.txt
+

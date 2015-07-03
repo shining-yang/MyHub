e95f3338fa82fc9eb8244782d6c24c9cb5f8e5d4
Shining <y.s.n@live.com>
2015-07-03 17:01:48 +0800

    Try to retrieve localhost IP

--- Snmp-Simulator/run.sh
+++ Snmp-Simulator/run.sh
@@ -8,26 +8,32 @@
 ##########################################################################
 
 
-if [ $# -lt 1 ]; then
-	echo "Require more arguments. Please specify the host IP address.";
-	echo "Example:";
-    echo "\t./THIS-SCRIPT  172.18.190.46";
-	exit 0
-fi
+#if [ $# -lt 1 ]; then
+#	echo "Require more arguments. Please specify the host IP address.";
+#	echo "Example:";
+#    echo "\t./THIS-SCRIPT  172.18.190.46";
+#	exit 0
+#fi
+
+##
+## TRY to auto-retrieve the IP address of localhost instead of specified by user
+##
+LOCALIP=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`
+echo "Use localhost IP: $LOCALIP"
+#exit 0
 
 # REMOVE the tmp directory so that we can start over again
 echo "### Removing tmp directory to start over ...";
-rm  -rf  tmp/
+rm  -rf  tmp
 
 # Generate simulator argument files
 echo "### Building simulator arguments ..."
-perl  build-simulator-args.pl  $1  50000  50255
+perl  build-simulator-args.pl  $LOCALIP  50000  50255
 
 # Start snmp simulators
 echo "### Building SNMP transport id ..."
-perl  build-transport-id.pl  ./static  1000  256
+perl  build-transport-id.pl  static  1000  256
 
 # READY to invoke
 echo "### Run SNMP simulators now ...";
-snmpsimd  --v2c-arch  --data-dir=./tmp  --transport-id-offset=1000  --args-from-file=./tmp/sim_port_50000_50255.txt
-
+snmpsimd  --v2c-arch  --data-dir=tmp  --transport-id-offset=1000  --args-from-file=tmp/sim_port_50000_50255.txt  &
